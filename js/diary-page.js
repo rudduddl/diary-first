4213f211202104682ad26aa0b0ee5598ed3ca128
rudduddl <kim5472678@gmail.com>
2024-05-19 23:14:54 +0900

    일기 삭제 기능 추가중

--- js/diary-page.js
+++ js/diary-page.js
@@ -31,3 +31,30 @@ function goToEditPage() {
   console.log("실행됨");
   window.open(url, "_blank");
 }
+
+// 이 함수는 예시로서, 기존의 일기 항목을 폼에 로드하는 방법을 보여줍니다.
+function loadDiaryEntry(key) {
+  let diaries = JSON.parse(localStorage.getItem("diaries")) || {};
+  if (diaries[key]) {
+    document.getElementById("date").value = diaries[key].date;
+    document.getElementById("title").value = diaries[key].title;
+    document.getElementById("content").value = diaries[key].content;
+    document.getElementById("diary-key").value = key;
+  }
+}
+
+//diary-key
+function deleteDiary() {
+  const key = document.getElementById("diary-key").value;
+  let diaries = JSON.parse(localStorage.getItem("diaries")) || {};
+
+  if (key && diaries[key]) {
+    delete diaries[key];
+    localStorage.setItem("diaries", JSON.stringify(diaries));
+    alert("일기가 성공적으로 삭제되었습니다.");
+    // 일기 목록 페이지로 이동 또는 다른 적절한 행동 수행
+    window.location.href = "home.html";
+  } else {
+    alert("일기를 찾을 수 없습니다.");
+  }
+}
